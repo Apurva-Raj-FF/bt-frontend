7dacb26c974d8a7e5d03447a4416bde79d0faa24
Apurva Raj <apurva.raj@fidelfolio.ai>
2025-10-03 20:13:41 +0530

    updated port to 8005
    updated port to 8005

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@
  * Base URL for the API server
  * @constant {string}
  */
-const BASE_URL = "http://localhost:8001/api";
+const BASE_URL = "http://localhost:8005/api";
 
 /**
  * Collection of all API endpoints used throughout the application
